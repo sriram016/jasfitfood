3adeaebb2a2c504e5aae302e34419afcdc103128
jayaram <sriramselvam016@gmail.com>
2025-10-07 16:19:12 +0530

    third commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,33 @@ $(document).ready(function(){
         }
     })
 });
+
+$(".testi1").owlCarousel({
+    center: true,
+    nav:false,
+    slideBy:1,
+    autoplay: true,
+    items: 3,
+    margin:20,
+    // animateIn:'fadeIn',
+    // animateOut:'fadeOut',
+    loop:true,
+    responsive:{
+        0:{
+            items:1,
+            nav: false,
+            dots:false
+        },
+        600:{
+            items:2,
+            nav:false,
+            dots:false
+        },
+        900:{
+            items:3,
+            nav: false,
+            dots:false
+        }
+        
+    }
+})
